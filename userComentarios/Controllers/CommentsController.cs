a23bc06c4e32d59b0d87e2aebc103bbfff7bc82a
AndersonAX4B <anderson.peruci@ax4b.com>
2022-05-17 13:22:25 -0300

    first commit

---
+++ userComentarios/Controllers/CommentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace userComentarios.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string user_name)
+        {
+            using (StreamReader r = new StreamReader(@"./Mock/mock.json"))
+            {
+                string json = r.ReadToEnd();
+
+                List<CommentsModel> result = new List<CommentsModel>();
+                List<CommentsModel> comments = JsonConvert.DeserializeObject<List<CommentsModel>>(json);
+
+                foreach (CommentsModel comment in comments.Where(c => c.nome_usuario == user_name))
+                {
+                    result.Add(comment);
+                }
+
+                return JsonConvert.SerializeObject(result);
+            }
+        }
+    }
+}
---
+++ userComentarios/Models/CommentsModel.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace userComentarios
+{
+    public class CommentsModel
+    {
+        
+        public string nome_usuario { get; set; }
+
+        public int comentario_id { get; set; }
+        
+        public string data { get; set; }
+        
+        public string comentario { get; set; }
+
+        public int usuario_id { get; set; }
+        
+
+    }
+}
